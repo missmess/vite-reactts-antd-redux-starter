d6ec57c76cfd743a80bb153c9009a047fce78162
wanglei <478271233@qq.com>
2022-01-25 15:56:55 +0800

    feat: optimize

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
   plugins: [react()],
   server: {
     open: true,
@@ -30,4 +30,4 @@ export default defineConfig({
       { find: /^@\//, replacement: path.join(__dirname, 'src/') },
     ],
   },
-});
+}));
