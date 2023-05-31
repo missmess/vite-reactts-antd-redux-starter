0a8f273a17dec1d2ff3dfb505b964c441ca970a9
wanglei <478271233@qq.com>
2023-05-31 15:07:51 +0800

    feat: bug调整

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Modal } from 'antd';
-import { setUser } from './store/user';
 import Loading from './components/loading';
 import store from './store';
-import { IDaasInfo } from './Constants';
-import { setScopes } from './store/scope';
+import { KEY_SETTING } from './Constants';
+import { parseJson } from './utils/common';
+import { setGlobalSetting, Setting } from './store/setting';
 
 const Home = lazy(() => import('@/pages/home'));
 
 // @ts-ignore
 // eslint-disable-next-line no-underscore-dangle
 window._redux_store = store;
+
+// 从localStorage中获取全局配置
+const settingStr = localStorage.getItem(KEY_SETTING);
+if (settingStr) {
+  const setting = parseJson(settingStr) as Setting;
+  store.dispatch(setGlobalSetting(setting));
+}
 
 export default () => {
   return (
--- src/Constants.ts
+++ src/Constants.ts
@@ -1,15 +1,4 @@
 // ======localStorage存储项
 export const KEY_SETTING = 'startter-setting';
 
-export const IDaasInfo = {
-  ontest: {
-    CLIENT_ID: '4mpEJzNRZDebTI7HevAyBB',
-    SERVICE_ID: '',
-  },
-  prod: {
-    CLIENT_ID: '4mpEJzNRZDebTI7HevAyBB',
-    SERVICE_ID: '',
-  },
-};
-
 export const OTHER = undefined;
